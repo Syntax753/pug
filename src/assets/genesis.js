e5952938cf4301a0039931ef597c8858f876a34e
Peter Turner <syntax.valid@gmail.com>
2025-10-18 15:48:28 +0100

    Renamed tile-splitter to genesis

--- src/assets/genesis.js
+++ src/assets/genesis.js
@@ -6,6 +6,7 @@ import sharp from 'sharp';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { exit } from 'process';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -44,6 +45,7 @@ async function splitTiles() {
             height: TILE_HEIGHT,
           })
           .toFile(outputFile);
+          exit();
       }
     }
 
