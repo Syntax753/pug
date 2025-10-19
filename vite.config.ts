e575ea9575d26caab8aca890d8caa4444fc9eae0
Peter Turner <syntax.valid@gmail.com>
2025-10-19 14:12:34 +0100

    Use default vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: './',
+    base: env.VITE_BASE_URL || './',
     plugins: [react()],
     css: {
       modules: {
@@ -17,8 +17,7 @@ export default defineConfig(({mode}) => {
     },
     build: { 
       sourcemap: true, 
-      manifest: true,
-      chunkSizeWarningLimit: 7000,
+      manifest: true
     },
     test: {
       environment: 'node',
