3f40b3f972ad666862a239792b6ffae8087852cf
Peter Turner <syntax.valid@gmail.com>
2025-10-19 13:50:09 +0100

    Revert vite config for local building

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,28 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import * as path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: { port: 3000 },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    base: './',
+    plugins: [react()],
+    css: {
+      modules: {
+        scopeBehaviour: 'local',
+      }
     },
-  },
+    server: { port: 3000 },
+    resolve: {
+      alias: { '@': '/src' }
+    },
+    build: { 
+      sourcemap: true, 
+      manifest: true,
+      chunkSizeWarningLimit: 7000,
+    },
+    test: {
+      environment: 'node',
+      globals: true
+    }
+  };
 });
